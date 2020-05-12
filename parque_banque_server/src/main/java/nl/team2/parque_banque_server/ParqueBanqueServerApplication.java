adec456dbdf7f6ffe80ecbe340baebfaf9ff0bb1
Machiel van Bruggen <machiel.van.bruggen@hva.nl>
2020-05-12 14:50:42 +0200

    startproject gemaakt

---
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/ParqueBanqueServerApplication.java
@@ -0,0 +1,13 @@
+package nl.team2.parque_banque_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueBanqueServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParqueBanqueServerApplication.class, args);
+    }
+
+}
---
+++ parque_banque_server/src/test/java/nl/team2/parque_banque_server/ParqueBanqueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.team2.parque_banque_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueBanqueServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
