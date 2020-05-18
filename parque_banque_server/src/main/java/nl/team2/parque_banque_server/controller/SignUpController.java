406da6c9d77f8b04051cc1404c42d7167d99a62f
Machiel van Bruggen <machiel.van.bruggen@hva.nl>
2020-05-18 17:19:11 +0200

    eerste begin van signup

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/SignUpController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/SignUpController.java
@@ -1,4 +1,27 @@
 package nl.team2.parque_banque_server.controller;
 
+import nl.team2.parque_banque_server.utilities.SignUpFormBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class SignUpController {
+
+    @GetMapping("/signup")
+    public String signupHandler(Model model) {
+        // add user object
+        SignUpFormBean signUpFormBean = new SignUpFormBean();
+        model.addAttribute(signUpFormBean);
+
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupConfirmation(@ModelAttribute SignUpFormBean signUpFormBean, Model model) {
+        model.addAttribute(signUpFormBean);
+        return "confirmsignup";
+    }
 }
--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/utilities/SignUpFormBean.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/utilities/SignUpFormBean.java
@@ -1,4 +1,47 @@
 package nl.team2.parque_banque_server.utilities;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class SignUpFormBean {
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @Size(min = 2, max = 10)
+    private String infix;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    public SignUpFormBean() {
+        super();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getInfix() {
+        return infix;
+    }
+
+    public void setInfix(String infix) {
+        this.infix = infix;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
