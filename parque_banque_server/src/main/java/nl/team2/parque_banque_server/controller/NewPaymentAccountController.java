034b59367d6820caafe7a88a346cd407285792ef
moraad <you@example.com>
2020-05-18 17:20:45 +0200

    rekining openen pagina met 2 links naar zakelijk en particulier pagina

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewPaymentAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewPaymentAccountController.java
@@ -1,4 +1,29 @@
 package nl.team2.parque_banque_server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class NewPaymentAccountController {
+
+    public NewPaymentAccountController() {
+    }
+
+    @GetMapping("rekeningopenen")
+    public String newPaymentAccount(){
+        return "newpaymentaccount";
+    }
+
+    @GetMapping("particuliereaccount")
+    public String newPrivateAccount(){
+        return "newprivateaccount";
+    }
+
+    @GetMapping("zakelijkeaccount")
+    public String newCompanyAccount(){
+        return "newcompanyaccount";
+    }
 }
+
+
