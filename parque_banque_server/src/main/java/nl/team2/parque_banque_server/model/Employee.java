7d1f0a3514b0b6b8acd01b3cc3e6f428a2a1f1aa
L.J. Kemeling <lisa.kemeling@hva.nl>
2020-05-20 11:09:13 +0200

    Aanpassingen classdiagram' git status q

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/Employee.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/Employee.java
@@ -1,4 +1,78 @@
 package nl.team2.parque_banque_server.model;
 
-public class Employee {
+import java.util.Objects;
+
+public class Employee extends User {
+
+    private Long id;
+    private String password;
+    private Role role;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String surName, String firstName, String affix, String phoneNumber, String eMailAddress, Address address) {
+        super(surName, firstName, affix, phoneNumber, eMailAddress, address);
+    }
+
+    public Employee(String surName, String firstName, String affix, String phoneNumber, String eMailAddress, Address address, String password, Role role) {
+        super(surName, firstName, affix, phoneNumber, eMailAddress, address);
+        this.password = password;
+        this.role = role;
+    }
+
+    public Employee( String password, Role role) {
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        if (!super.equals(o)) return false;
+        Employee employee = (Employee) o;
+        return getId().equals(employee.getId()) &&
+                getPassword().equals(employee.getPassword()) &&
+                getRole().equals(employee.getRole());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getId(), getPassword(), getRole());
+    }
 }
