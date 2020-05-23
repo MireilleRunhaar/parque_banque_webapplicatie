f5ac425b6786b444af11d312a2504a2c9e7faccc
Mireille <mireille.runhaar@gmail.com>
2020-05-23 15:31:03 +0200

    Klassen: zakelijke rekening en accountmanager toegevoegd, app props aangepast naar update

---
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/BusinessAccount.java
@@ -0,0 +1,8 @@
+package nl.team2.parque_banque_server.model;
+
+public class BusinessAccount extends PaymentAccount {
+
+    private Company company;
+    private AccountManager accountManager;
+
+}
