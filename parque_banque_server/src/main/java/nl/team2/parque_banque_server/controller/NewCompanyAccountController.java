f3374ed918a2c407dc3078de859c650b583bfcf4
Mireille <mireille.runhaar@gmail.com>
2020-05-25 18:28:08 +0200

    NewCompanyAccountController invulformulier voor aanmelden nieuw bedrijf

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewCompanyAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewCompanyAccountController.java
@@ -1,21 +1,25 @@
 package nl.team2.parque_banque_server.controller;
 
+import nl.team2.parque_banque_server.model.Company;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class NewCompanyAccountController {
 
-    //3 scenario's mogelijk:
     //Klant is via een rekening gekoppeld aan een(of meer) bedrijf(ven)
     //Klant wil voor een van zijn/haar bedrijven een nieuwe rekening openen
-
     //Tonen pagina
     @GetMapping("/zakelijke-rekening-openen")
     public String showNewCompanyAccount(){
         return "zakelijke-rekening-openen";
     }
-
     //Uit de DB de bedrijven te halen van de ingelogde klant
 
     //Klant is via een rekening gekoppeld aan een(of meer) bedrijf(ven)
@@ -23,9 +27,21 @@ public class NewCompanyAccountController {
 
     //Klant heeft nog geen zakelijke rekening(en)
     //Klant wil een nieuw bedrijf aanmaken bij Parque Banque en een rekening openen
-
-    //Tonen van de pagina met invulformulier
-
+    //Tonen van de pagina newcompanyaccount met invulformulier
+    @GetMapping("/newcompanyaccount")
+    public String showNewCompanyAccount(Model model) {
+        Company company = new Company();
+        model.addAttribute("company", company);
+        //List<String> sectoren = Arrays.asList("Handel", "Industrie", "Zorg");
+        //model.addAttribute("sector", sectoren);
+        return "newcompanyaccount";
+    }
+    //Tonen van het ingevulde formulier op de confirmcompany pagina
+    @PostMapping("/newcompanyaccount")
+    public String submitForm(@ModelAttribute("company") Company company) {
+        System.out.println(company);
+        return "confirmcompany";
+    }
 
 
 
