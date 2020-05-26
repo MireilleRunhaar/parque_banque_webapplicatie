4b23a434a41e477b0e602397fd5b4fb34bf81ed3
minke <minke.pronk@upcmail.nl>
2020-05-26 13:22:05 +0200

    repo en service paymentaccount uitgegrijsd vanwege foutmelding

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/PaymentAccountService.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/PaymentAccountService.java
@@ -1,28 +1,28 @@
-package nl.team2.parque_banque_server.service;
-
-import nl.team2.parque_banque_server.model.Customer;
-import nl.team2.parque_banque_server.model.PaymentAccount;
-import nl.team2.parque_banque_server.repository.PaymentAccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class PaymentAccountService {
-
-    @Autowired
-    private PaymentAccountRepository paymentAccountRepo;
-
-    public PaymentAccountService(){
-        super();
-    }
-
-    //TO DO : maak methode om een lijst te krijgen van alle accountholders op te halen behorende bij de username
-    //haal ook de iban en balanceCent op.
-
-    public PaymentAccount findPaymentAccount(List<Customer> accountHolders){
-        return paymentAccountRepo.findPaymentAccountsByAccountHolders(accountHolders);
-
-    }
-}
+//package nl.team2.parque_banque_server.service;
+//
+//import nl.team2.parque_banque_server.model.Customer;
+//import nl.team2.parque_banque_server.model.PaymentAccount;
+//import nl.team2.parque_banque_server.repository.PaymentAccountRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class PaymentAccountService {
+//
+//    @Autowired
+//    private PaymentAccountRepository paymentAccountRepo;
+//
+//    public PaymentAccountService(){
+//        super();
+//    }
+//
+//    //TO DO : maak methode om een lijst te krijgen van alle accountholders op te halen behorende bij de username
+//    //haal ook de iban en balanceCent op.
+//
+//    public PaymentAccount findPaymentAccount(List<Customer> accountHolders){
+//        return paymentAccountRepo.findPaymentAccountsByAccountHolders(accountHolders);
+//
+//    }
+//}
