da7f1e6816743e8ea3716658909375dda8c86146
Sanne <sannewinkelman@hotmail.com>
2020-05-27 16:01:41 +0200

    LoginCustomer, onnodige imports weggehaald

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LoginCustomerController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LoginCustomerController.java
@@ -1,11 +1,9 @@
 package nl.team2.parque_banque_server.controller;
 
 import nl.team2.parque_banque_server.model.Customer;
-import nl.team2.parque_banque_server.model.Employee;
 import nl.team2.parque_banque_server.service.CustomerService;
 import nl.team2.parque_banque_server.service.LoginService;
 import nl.team2.parque_banque_server.utilities.LoginCustomerFormBean;
-import nl.team2.parque_banque_server.utilities.LoginEmployeeFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.management.Attribute;
 import javax.validation.Valid;
-import java.util.Map;
 
 @Controller
 @SessionAttributes("customerId")
--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/utilities/LoginCustomerFormBean.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/utilities/LoginCustomerFormBean.java
@@ -1,9 +1,6 @@
 package nl.team2.parque_banque_server.utilities;
 
-import nl.team2.parque_banque_server.model.PaymentAccount;
-
 import javax.validation.constraints.NotBlank;
-import java.util.List;
 
 public class LoginCustomerFormBean {
 
