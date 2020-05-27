0a61e0af5bf13c4874dda422e9c27c7a7235797c
Sanne <sannewinkelman@hotmail.com>
2020-05-27 12:25:42 +0200

    LoginEmployee aangepast voor communicatie juiste gegevens.

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LoginCustomerController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LoginCustomerController.java
@@ -1,11 +1,14 @@
 package nl.team2.parque_banque_server.controller;
 
 import nl.team2.parque_banque_server.model.Customer;
+import nl.team2.parque_banque_server.model.Employee;
 import nl.team2.parque_banque_server.service.CustomerService;
 import nl.team2.parque_banque_server.service.LoginService;
 import nl.team2.parque_banque_server.utilities.LoginCustomerFormBean;
+import nl.team2.parque_banque_server.utilities.LoginEmployeeFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.management.Attribute;
 import javax.validation.Valid;
 
 @Controller
@@ -26,24 +30,31 @@ public class LoginCustomerController {
     private LoginService loginService;
 
     @GetMapping("/inloggen")
-    public ModelAndView loginHandler(@ModelAttribute LoginCustomerFormBean loginCustomerFormBean) {
-        ModelAndView mav = new ModelAndView("logincustomer");
-        mav.addObject("customer", loginCustomerFormBean);
-        return mav;
+    public String loginHandler(@ModelAttribute LoginCustomerFormBean loginCustomerFormBean,
+                               Model model) {
+        Object id = model.getAttribute("customerId");
+
+        /*Employee employee = employeeService.findByEmployeeNumber(loginEmployeeFormBean.getEmployeeNumber());*/
+
+        if (model.containsAttribute("customerId")) {
+            return "accountview";
+        } else {
+            model.addAttribute("customerId", loginCustomerFormBean);
+            return "logincustomer";
+        }
     }
 
     @PostMapping("/inloggen")
-    public ModelAndView customerLoginFormHandler(@Valid LoginCustomerFormBean loginCustomerFormBean, BindingResult bindingResult) {
-        ModelAndView mav = new ModelAndView();
-
+    public String customerLoginFormHandler(@Valid LoginCustomerFormBean loginCustomerFormBean,
+                                                 BindingResult bindingResult,
+                                                 Model model) {
         if (bindingResult.hasErrors() || !loginService.customerLoginValidation(loginCustomerFormBean)){
-            mav.addObject("invalidCredentials", true);
-            mav.setViewName("logincustomer");
+            model.addAttribute("invalidCredentials", true);
+            return "logincustomer";
         } else {
             Customer customer = customerService.findByUserName(loginCustomerFormBean.getUserName());
-            mav.addObject("customerId", customer.getId());
-            mav.setViewName("accountview");
+            model.addAttribute("customerId", customer.getId());
+            return "accountview";
         }
-        return mav;
     }
 }
