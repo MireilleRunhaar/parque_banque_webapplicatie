08e753670717ccbaafffd2afe748cf405abd021a
minke <minke.pronk@upcmail.nl>
2020-05-28 11:37:16 +0200

    details2

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/AccountViewController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/AccountViewController.java
@@ -27,7 +27,7 @@ public class AccountViewController {
         }
         else {
             long customerId = (long) model.getAttribute("customerId");
-            Customer customer = customerService.findCustomerById(customerId);
+            Customer customer = customerService.findCustomerByCustomerId(customerId);
             model.addAttribute("name", customer.getFirstName());
             model.addAttribute("paymentAccounts", customer.getPaymentAccounts());
             return "accountview";
