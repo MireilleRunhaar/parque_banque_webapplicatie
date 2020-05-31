c21ce7aa4f816bd342b975689484f649097891ab
Sanne <sannewinkelman@hotmail.com>
2020-05-31 11:49:12 +0200

    Aanpassingen EmployeeRepository

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/repositories/EmployeeRepository.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/repositories/EmployeeRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends  CrudRepository<Employee, Integer> {
+public interface EmployeeRepository extends  CrudRepository<Employee, Long> {
 
     Employee findEmployeeByEmployeeNumber(int employeeNumber);
-
-    Employee findEmployeeById(long id);
 }
