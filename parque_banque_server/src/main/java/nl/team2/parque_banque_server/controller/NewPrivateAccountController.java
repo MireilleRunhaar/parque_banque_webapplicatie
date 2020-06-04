3363321e586c95af520e819cdd5afdde230b3d3a
M. Anas <moraad.anas@hva.nl>
2020-06-04 13:11:10 +0200

    Update parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewPrivateAccountController.java

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewPrivateAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/NewPrivateAccountController.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 @SessionAttributes("customerId") // key -value
 public class NewPrivateAccountController {
 
-    public final long START_SALDO=2500;
+    public final long START_SALDO=2500L;
 
     private PaymentAccountService.IbanService ibanService;
     private CustomerService customerService;
