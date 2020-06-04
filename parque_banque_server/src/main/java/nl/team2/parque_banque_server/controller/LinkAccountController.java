7837c9bddd1457cdeab05b435c5445f618b2d29e
minke <minke.pronk@upcmail.nl>
2020-06-04 19:27:12 +0200

    linkaccountcontroller verder ingevuld

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LinkAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LinkAccountController.java
@@ -1,4 +1,20 @@
 package nl.team2.parque_banque_server.controller;
 
+import nl.team2.parque_banque_server.model.Authorisation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("customerId")
 public class LinkAccountController {
+
+    @GetMapping("/rekening-toevoegen")
+    public ModelAndView linkAccountHandler(Model model, Authorisation authorisation){
+        ModelAndView mav= new ModelAndView();
+        return mav;
+    }
+
 }
