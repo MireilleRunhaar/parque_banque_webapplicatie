7d62b19ec51445626f4f2728aa18df6490d20cd3
Mireille <mireille.runhaar@gmail.com>
2020-06-05 17:38:33 +0200

    Voorlopige getmapping van rekeningkeuze

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/SelectedAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/SelectedAccountController.java
@@ -1,7 +1,16 @@
 package nl.team2.parque_banque_server.controller;
 
+import nl.team2.parque_banque_server.model.BusinessAccount;
+import nl.team2.parque_banque_server.model.Customer;
+import nl.team2.parque_banque_server.model.Employee;
+import nl.team2.parque_banque_server.model.PaymentAccount;
+import nl.team2.parque_banque_server.service.CustomerService;
+import nl.team2.parque_banque_server.service.PaymentAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -10,15 +19,31 @@ import java.time.format.DateTimeFormatter;
 
 
 @Controller
-//Todo: Add SessionAttributes: IBAN NR
+@SessionAttributes("iban")
 public class SelectedAccountController {
 
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    PaymentAccountService.IbanService ibanService;
+
+    //Rekeningpagina ophalen en ingelogde klant meegeven als session attribute
     @GetMapping("/selectedaccount")
-    public String showSelectedAccountPageHandler(){
-        return "selectedaccount";
+    public String showSelectedAccountPageHandler(Model model){
+        if(!model.containsAttribute("customerId")) {
+            return "logincustomer";
+        } else {
+            Customer customer = customerService.findCustomerBySAId(model.getAttribute("customerId"));
+            model.addAttribute("customer", customer);
+          //  model.addAttribute("iban", )
+            model.addAttribute("DatumEnTijd", getCurrentTimeWithTimeZone());
+            return "selectedaccount";
+        }
     }
 
     //Van de geselecteerde rekeningnummer ophalen: rekeningnummer, tenaamstelling, saldo en de laatste 10 transacties.
+
     //Huidige datum en tijd weergeven
     public static String getCurrentTimeWithTimeZone(){
         ZoneId zoneId = ZoneId.of("Europe/Paris");
