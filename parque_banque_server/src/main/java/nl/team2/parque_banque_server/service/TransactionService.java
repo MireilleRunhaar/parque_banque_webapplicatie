20913be616730d323d8db8791fd6dfca59376e53
Lisa Kemeling <lisakemeling@gmail.com>
2020-06-07 21:43:24 +0200

    twee testen voor TransactionService
    een integratietest voor TransactionService
    een ServiceTest met Mock
    ToDo twee testen waarin het niet zou kunnen werken "gekke data"

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/TransactionService.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/TransactionService.java
@@ -13,12 +13,16 @@ import java.time.LocalDate;
 @Service
 public class TransactionService {
 
-    @Autowired
     private PaymentAccountRepository paymentAccountRepo;
-    @Autowired
     private TransactionRepository transactionRepo;
+    private PaymentAccountService paymentAccountService;
+
     @Autowired
-    PaymentAccountService paymentAccountService;
+    public TransactionService(PaymentAccountRepository paymentAccountRepo, TransactionRepository transactionRepo, PaymentAccountService paymentAccountService) {
+        this.paymentAccountRepo = paymentAccountRepo;
+        this.transactionRepo = transactionRepo;
+        this.paymentAccountService = paymentAccountService;
+    }
 
 
     public void executeAndSave(Transaction transaction){
@@ -33,6 +37,20 @@ public class TransactionService {
             PaymentAccount creditAccount = paymentAccountService.findOneByIban(transactionFormBean.getIbanCreditAccount());
             PaymentAccount debitAccount = paymentAccountService.findOneByIban(ibanDebit);
             return new Transaction(amountCent, transactionFormBean.getDescription(), LocalDate.now(), creditAccount , debitAccount);
+    }
+
+    public PaymentAccountRepository getPaymentAccountRepo() {
+        return paymentAccountRepo;
+    }
+
+
+    public TransactionRepository getTransactionRepo() {
+        return transactionRepo;
+    }
+
+
+    public PaymentAccountService getPaymentAccountService() {
+        return paymentAccountService;
     }
 
 }
