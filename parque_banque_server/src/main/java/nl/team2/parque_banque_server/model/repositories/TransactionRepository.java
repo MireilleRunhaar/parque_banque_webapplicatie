3daa6351f9d290998ce32f8967eecf4a4b8085b1
Lisa Kemeling <lisakemeling@gmail.com>
2020-06-08 08:52:09 +0200

    Query voor ophalen history

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/repositories/TransactionRepository.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/model/repositories/TransactionRepository.java
@@ -1,8 +1,13 @@
 package nl.team2.parque_banque_server.model.repositories;
 
+import nl.team2.parque_banque_server.model.PaymentAccount;
 import nl.team2.parque_banque_server.model.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 
+    List<Transaction> findTop10ByCreditAccountOrderByDateAsc(PaymentAccount creditAccount);
+    List<Transaction> findTop10ByDebitAccountOrderByDateAsc(PaymentAccount debitAccount);
 }
