798ebed3bd26b541090b7579f59565d73b562639
minke <minke.pronk@upcmail.nl>
2020-06-09 20:29:14 +0200

    aanpassingen in linkaccountservice en controller

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LinkAccountController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/LinkAccountController.java
@@ -2,8 +2,12 @@ package nl.team2.parque_banque_server.controller;
 
 import nl.team2.parque_banque_server.model.Authorisation;
 import nl.team2.parque_banque_server.model.Customer;
+import nl.team2.parque_banque_server.model.PaymentAccount;
 import nl.team2.parque_banque_server.service.AuthorisationService;
+import nl.team2.parque_banque_server.service.CustomerService;
 import nl.team2.parque_banque_server.service.LinkAccountService;
+import nl.team2.parque_banque_server.service.PaymentAccountService;
+import nl.team2.parque_banque_server.utilities.AddAccountHolderFormBean;
 import nl.team2.parque_banque_server.utilities.LinkAccountFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +30,10 @@ public class LinkAccountController {
     private AuthorisationService authorisationService;
     @Autowired
     private LinkAccountService linkAccountService;
+    @Autowired
+    private CustomerService customerService;
+    @Autowired
+    private PaymentAccountService paymentAccountService;
 
     //controleer of de customer is ingelogd. Zo nee, ga dan naar de login pagina.
     //if customer is logged in, then fill iban and safetycode in.
@@ -43,18 +51,24 @@ public class LinkAccountController {
 
     //checking for login credentials
     @PostMapping("/rekening-toevoegen")
-    public String linkAccount(@Valid LinkAccountFormBean linkAccountFormBean, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors() || linkAccountService.linkAccountValidation(linkAccountFormBean)) {
+    public ModelAndView linkAccount(@Valid LinkAccountFormBean linkAccountFormBean, AddAccountHolderFormBean addAccountHolderFormBean, BindingResult bindingResult, PaymentAccount paymentAccount, Model model) {
+        ModelAndView mav = new ModelAndView();
+        //input voor linkaccountvalidation is linkaccountformbean en addaccountholderformbean.
+        if (bindingResult.hasErrors() || !linkAccountService.linkAccountValidation(linkAccountFormBean, addAccountHolderFormBean)) {
             System.out.println("fouten");
             model.addAttribute("invalidCredentials", true);
-            return "linkpaymentaccount";
+            mav.setViewName("linkpaymentaccount");
         } else {
-            List<Authorisation> authorisationList = authorisationService.findAllByUserName(linkAccountFormBean.getIban());
-            model.addAttribute("customerId", authorisationList);
-            return "linkpaymentaccountconfirmation";
+            //als validatie goed is voeg dan de klant toe// dit gaat niet goed.  Long(id) toegevoegd omdat id anders niet goed gaat.
+            long id = (long) model.getAttribute("customerId");
+            Customer customer = customerService.findById(id);
+            //customer toevoegen aan de lijst met accountholders
+            paymentAccount.addCustomerToAccountHolder(customer);
+            mav.setViewName("linkpaymentaccountconfirmation");
 
         }
-
+        return mav;
     }
+
+
 }
--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/LinkAccountService.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/service/LinkAccountService.java
@@ -1,6 +1,8 @@
 package nl.team2.parque_banque_server.service;
 
 import nl.team2.parque_banque_server.model.Authorisation;
+import nl.team2.parque_banque_server.model.Customer;
+import nl.team2.parque_banque_server.utilities.AddAccountHolderFormBean;
 import nl.team2.parque_banque_server.utilities.LinkAccountFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,21 +16,23 @@ public class LinkAccountService {
     @Autowired
     AuthorisationService authorisationService;
 
-    public boolean linkAccountValidation(LinkAccountFormBean linkAccountFormBean) {
-        //maak een lijst met autorisaties. Haal deze op op basis van de username en het ibannummer
-        List<Authorisation> authorisationList = authorisationService.findAllByUserName(linkAccountFormBean.getIban());
+    public boolean linkAccountValidation(LinkAccountFormBean linkAccountFormBean, AddAccountHolderFormBean addAccountHolderFormBean) {
+        // haal een lijst op uit de database op basis van de username uit de accountholderformbean
+        //als er geen authorisaties zijn dan is het resultaat false.
+        List<Authorisation> authorisationList = authorisationService.findAllByUserName(addAccountHolderFormBean.getUsername());
         if (authorisationList.size() == 0) {
             return false;
         }
-        //check for every iban of de security code uit de db overeenkomt met de securitycode in de bean.
-        //check for evetsame iban. If not present then return false else check for safetycode
-        // Voor elke iban op de lijst check of de securitycode uit de db overeenkomt met de ingevulde.
+        // Voor elke iban op de lijst check of de iban uit de db overeenkomt met de iban uit de bean. 
+        // en check of de securitycode overeenkomt met de securitycode uit de bean.
+        // zo ja, return true, zo nee return false
         else {
             for (Authorisation authorisation : authorisationList) {
-                return authorisation.getSecurityCode().equals(linkAccountFormBean.getSecurityCode());
-
+                return authorisation.getSecurityCode().equals(linkAccountFormBean.getSecurityCode())
+                        && authorisation.getIban().equals(linkAccountFormBean.getIban());
             }
-            return true;
+            return false;
         }
     }
+
 }
