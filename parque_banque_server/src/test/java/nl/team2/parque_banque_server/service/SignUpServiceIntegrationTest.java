7cebd45581375b291f157a0cb60fbe0023fab4d1
Machiel van Bruggen <machiel.van.bruggen@hva.nl>
2020-06-09 14:39:16 +0200

    service test en kale integration test

--- parque_banque_server/src/test/java/nl/team2/parque_banque_server/service/SignUpServiceIntegrationTest.java
+++ parque_banque_server/src/test/java/nl/team2/parque_banque_server/service/SignUpServiceIntegrationTest.java
@@ -32,21 +32,4 @@ public class SignUpServiceIntegrationTest {
         assertNotNull(signUpService.getCustomerService().getCustomerRepo());
     }
 
-    @Test
-    void testCustomerService() {
-        // usernames that exist in database (users with id 1-6):
-        assertTrue(signUpService.isUserNameTaken("dmcelree0"));
-        assertTrue(signUpService.isUserNameTaken("scollingwood1"));
-        assertTrue(signUpService.isUserNameTaken("sterne2"));
-        assertTrue(signUpService.isUserNameTaken("dscogin3"));
-        assertTrue(signUpService.isUserNameTaken("yshadbolt4"));
-        assertTrue(signUpService.isUserNameTaken("ngrimshaw5"));
-
-        // usernames that do not exist in database:
-        assertFalse(signUpService.isUserNameTaken("abcde"));
-        assertFalse(signUpService.isUserNameTaken("qwerty"));
-        assertFalse(signUpService.isUserNameTaken("markruttesaccount"));
-        assertFalse(signUpService.isUserNameTaken("ikbencoronabeu"));
-        assertFalse(signUpService.isUserNameTaken("hvamiwc19"));
-    }
 }
