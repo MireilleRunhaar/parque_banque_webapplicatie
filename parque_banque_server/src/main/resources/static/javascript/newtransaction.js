ad635244222c681d5bc149ce83d5422f5b423766
Lisa Kemeling <lisakemeling@gmail.com>
2020-06-10 19:21:38 +0200

    het formulier kan weer gesubmit worden, alleen de errors blijven staan

--- parque_banque_server/src/main/resources/static/javascript/newtransaction.js
+++ parque_banque_server/src/main/resources/static/javascript/newtransaction.js
@@ -3,76 +3,80 @@ const amount = document.getElementById('amount');
 const cents = document.getElementById('cents');
 const iban = document.getElementById('iban');
 const description = document.getElementById('description');
+const MAX_TRANSACTION = 10000000;
+const MIN_TRANSACTION = 0;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    checkInputs();
-    
+    if(checkInputs() < 1){
+        form.submit();
+    }
+
 })
 
 function checkInputs(){
-    // de waarden ophalen uit de inputs
-    let amountInput = amount.value;
-    let centsInput = cents.value;
+    const amountInput = amount.value;
+    const centsInput = cents.value;
     const ibanInput = iban.value.trim();
     const descriptionInput = description.value.trim();
+    let errors = 0;
 
 
-    if(validateTotalAmount()){
-        setSuccesFor(amount)
+    if(!validateTotalAmount(amountInput, centsInput)){
+        setErrorFor(amount, "Vul een bedrag in");
+        errors++;
     } else{
-        setErrorFor(amount, "Vul een bedag in")
+        setSuccesFor(amount);
     }
 
-    /*if(centsInput === '' || amountInput === null){
-        // show error
-        // add error class
-        setErrorFor(amount, 'Vul een bedrag in');
-    } else {
-        // add succes class
-        setSuccesFor(amount);
-    }*/
-    
-    if(ibanInput === ''){
-        setErrorFor(iban, 'Vul een rekeningnummer in')
+    if(ibanInput === '' || ibanInput === null){
+        setErrorFor(iban, 'Vul een rekeningnummer in');
+        errors++;
     } else if(!validIban(ibanInput)){
         setErrorFor(iban, 'Vul een geldig rekeningnummer in (IBAN)');
+        errors++;
     } else {
         setSuccesFor(iban);
     }
+
+    if(descriptionInput.length > 140){
+        setErrorFor(description, "U kunt maximaal 140 tekens invoeren");
+        errors++;
+    } else{
+        setSuccesFor(description);
+    }
+
+    return errors;
 }
 
 function setErrorFor(input, message){
-    const formControl = input.parentElement; //form-control
+    const formControl = input.parentElement;
     const small = formControl.querySelector('small');
-    
-    // add error message inside small
     small.innerText = message;
-    
-    // add error class for CSS
     formControl.className = 'form-control error';
+
 }
 
 function setSuccesFor(input){
-    const formControl = input.querySelector('small');
+    const formControl = input.parentElement;
     formControl.className = 'form-control succes';
-    
 }
 
 function validIban (iban){
     return /NL\d{2}PARQ0\d{9}/.test(iban);
 }
 
+
+
+
 function validateTotalAmount(amountInput, centsInput){
-    if(centsInput != null && amountInput != null){
+    if(centsInput !== null && amountInput !== null){
         centsInput = Number(centsInput);
         amountInput = Number(amountInput);
         if(Number.isInteger(centsInput) && Number.isInteger(amountInput)){
             const totalAmount = (amountInput * 100) + centsInput;
-            if(totalAmount > 0 && totalAmount <= 10000000){
-                return true;
-            }
+            console.log(totalAmount);
+            return totalAmount > MIN_TRANSACTION && totalAmount <= MAX_TRANSACTION;
         } else{
             return false;
         }
