3b8e06484fe1be37170844529def40dfb532cb2c
moraad <you@example.com>
2020-06-14 16:46:56 +0200

    overviewbusinesscontroller hoogste saldo's  +knop werkend

--- parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/OverviewBusinessAccountsController.java
+++ parque_banque_server/src/main/java/nl/team2/parque_banque_server/controller/OverviewBusinessAccountsController.java
@@ -1,12 +1,20 @@
 package nl.team2.parque_banque_server.controller;
 
+import nl.team2.parque_banque_server.model.BusinessAccount;
 import nl.team2.parque_banque_server.model.Employee;
 import nl.team2.parque_banque_server.service.EmployeeService;
+import nl.team2.parque_banque_server.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @SessionAttributes("employeeId")
@@ -14,6 +22,8 @@ public class OverviewBusinessAccountsController {
 
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private StatisticsService statisticsService;
 
     @GetMapping("/overzicht-bedrijven")
     public String handleOverviewBusiness(Model model) {
@@ -25,7 +35,35 @@ public class OverviewBusinessAccountsController {
         } else if (employee.getRole().getId() != 2)   {
             return "redirect:/personeel-home";
         } else {
-            return "overviewbusinessaccounts";
+            return "overviewbusinessaccount";
         }
     }
+
+    @RequestMapping(value = "/overzicht-bedrijven", params = "Saldo", method = RequestMethod.POST)
+    public ModelAndView top10BalanceBusinessAccounts(){
+        ModelAndView mav=new ModelAndView("tenrichestbusinessaccounts");
+
+
+        Map<Long,Object[]> richestBusinessCustomers=statisticsService.getTenRichestBusinessCustomers();
+
+        mav.addObject("richestBusinessCustomers",richestBusinessCustomers);
+
+        return mav;
+    }
+
+//    //Sector
+//    @RequestMapping(value = "/overzicht-bedrijven", params = "Sectoren", method = RequestMethod.POST)
+//    public ModelAndView averageBalanceSector(){
+//        ModelAndView mav=new ModelAndView("averagebalancesectors");
+//
+//        //List<BusinessAccount> businessAccountsList=businessAccountService.findAll();
+//
+//       // Map<String,Long> sectorAverageBalance= businessAccountService.averageBalanceSector(businessAccountsList);
+//
+//       // mav.addObject("averageBalance",sectorAverageBalance);
+//
+//        return mav;
+//    }
+
 }
+
